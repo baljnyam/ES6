2b87f77e62b2d4d4de8cc63b25377836fb87e254
baljnyam <baljnyam@gmail.com>
2019-12-27 10:12:15 +1100

    initial commit

--- clien-side-web-API.js
+++ clien-side-web-API.js
@@ -1,42 +1,40 @@
+// Create an AudioContext (cross browser)
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
-
-const audioElement = document.querySelector('audio')
-const playBtn = document.querySelector('button')
-const volumeSlider = document.querySelector('.volume')
-
-const audioSource = audioCtx.createMediaElementSource(audioElement)
-
+// store references to our HTML elements
+const audioElement = document.querySelector('audio');
+const playBtn = document.querySelector('button');
+const volumeSlider = document.querySelector('.volume');
+// load the audio source into our audio graph
+const audioSource = audioCtx.createMediaElementSource(audioElement);
 // play/pause audio
-
 playBtn.addEventListener('click', function () {
   // check if context is in suspended state (autoplay policy)
   if (audioCtx.state === 'suspended') {
-    audioCtx.resume()
+    audioCtx.resume();
   }
   // if track is stopped, play it
   if (this.getAttribute('class') === 'paused') {
-    audioElement.play()
-    this.setAttribute('class', 'playing')
+    audioElement.play();
+    this.setAttribute('class', 'playing');
     this.textContent = 'Pause'
     // if track is playing, stop it
   } else if (this.getAttribute('class') === 'playing') {
-    audioElement.pause()
-    this.setAttribute('class', 'paused')
-    this.textContent = 'Play'
+    audioElement.pause();
+    this.setAttribute('class', 'paused');
+    this.textContent = 'Play';
   }
-})
-
+});
 // if track ends
 audioElement.addEventListener('ended', function () {
-  playBtn.setAttribute('class', 'paused')
+  playBtn.setAttribute('class', 'paused');
   this.textContent = 'Play'
-})
-
-const gainNode = audioCtx.createGain()
-
+});
+// volume
+const gainNode = audioCtx.createGain();
 volumeSlider.addEventListener('input', function () {
-  gainNode.gain.value = this.value
-})
-
-audioSource.connect(gainNode).connect(audioCtx.destination)
+  gainNode.gain.value = this.value;
+});
+// connect our graph
+audioSource.connect(gainNode).connect(audioCtx.destination);
+// Track credit: Outfoxing the Fox by Kevin MacLeod under Creative Commons
